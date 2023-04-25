d26bce0424e4d82ebed0d0fa79a3c01e39700dbd
vieirajon <jon.dingleberg@gmail.com>
2023-04-25 18:39:22 -0300

    Day 2

--- #1.py
+++ #1.py
@@ -1,3 +1,5 @@
+#GERADOR DE NOME DE BANDA
+
 print("Bem Vindo ao seu Gerador de Nome de Banda!")
 nome_cidade = input("Qual o nome da cidade onde você cresceu?\n")
 nome_pet = input("Qual o nome do seu pet quando criança?\n")
