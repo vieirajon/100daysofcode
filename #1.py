e14f43056c36f7720770401897b0d752b040836b
vieirajon <jon.dingleberg@gmail.com>
2023-04-25 17:02:29 -0300

    Initial commit

---
+++ #1.py
@@ -0,0 +1,4 @@
+print("Bem Vindo ao seu Gerador de Nome de Banda!")
+nome_cidade = input("Qual o nome da cidade onde você cresceu?\n")
+nome_pet = input("Qual o nome do seu pet quando criança?\n")
+print("O nome da sua banda seria " + nome_cidade + " " + nome_pet)
