f8a8fe894c4bc49037c4f5f344ca41c44a6367be
vieirajon <jon.dingleberg@gmail.com>
2023-04-27 19:07:56 -0300

    Dia 6

---
+++ #6.py
@@ -0,0 +1,20 @@
+#REBORG'S: LOST IN A MAZE
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
